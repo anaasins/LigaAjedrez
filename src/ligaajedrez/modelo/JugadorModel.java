d3956144f8792f3813d05de6f077077d7a4f57a2
Julio Beltran <juliofagotero@gmail.com>
2020-10-28 21:11:02 +0100

    Finished player model

--- src/ligaajedrez/modelo/JugadorModel.java
+++ src/ligaajedrez/modelo/JugadorModel.java
@@ -5,10 +5,86 @@
  */
 package ligaajedrez.modelo;
 
+import ligaajedrez.modelo.enums.CategoriaEnum;
+
 /**
  *
  * @author jbeltran
  */
 public class JugadorModel {
+    public String name;
+    public int elo;
+    //public Club club;
+    public int age;
+    public CategoriaEnum category;
+    public String responsableName;
+    public String reponsablePhoneNumber;
+
+    public JugadorModel() {
+    }
+
+    /*public JugadorModel(String name, int elo, int age, Club club, String responsableName, String reponsablePhoneNumber) {
+        setName(name);
+        setElo(elo);
+        setClub(club);
+        setResponsableName(responsableName);
+        setReponsablePhoneNumber(reponsablePhoneNumber);
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (!name.trim().isEmpty())
+            this.name = name;
+    }
+
+    public int getElo() {
+        return elo;
+    }
+
+    public void setElo(int elo) {
+        if (elo >= 0 && elo <= 3000)
+            this.elo = elo;
+    }
+
+    /*public Club getClub() {
+        return club;
+    }
     
+    public void setClub(Club club) {
+        this.club = club;
+    }*/
+    
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0 && age < 140)
+            this.age = age;
+    }
+
+    public CategoriaEnum getCategory() {
+        return category;
+    }
+
+    public String getResponsableName() {
+        return responsableName;
+    }
+
+    public void setResponsableName(String responsableName) {
+        if (!responsableName.trim().isEmpty())
+            this.responsableName = responsableName;
+    }
+
+    public String getReponsablePhoneNumber() {
+        return reponsablePhoneNumber;
+    }
+
+    public void setReponsablePhoneNumber(String reponsablePhoneNumber) {
+        if (reponsablePhoneNumber.matches("(+[0-9]{2})?[0-9]{9}"))
+            this.reponsablePhoneNumber = reponsablePhoneNumber;
+    }
 }
