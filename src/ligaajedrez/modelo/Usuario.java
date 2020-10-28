d847483345bb4915531c230ae745c1a2b3682dc6
Julio Beltran <juliofagotero@gmail.com>
2020-10-28 22:49:29 +0100

    Added base user for login

---
+++ src/ligaajedrez/modelo/Usuario.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ligaajedrez.modelo;
+
+/**
+ *
+ * @author jbeltran
+ */
+public class Usuario {
+    private String userName;
+    private String userPass;
+
+    public Usuario() {
+    }
+
+    public Usuario(String userName, String userPass) {
+        this.userName = userName;
+        this.userPass = userPass;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+}
