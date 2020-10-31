789955423c3fcb3f2103ab4e769b0219abb6260a
Julio Beltran <juliofagotero@gmail.com>
2020-10-31 20:38:22 +0100

    Add administrator and previousView to ReservarSedeEntrenamiento

--- src/ligaajedrez/vista/ReservarSedeEntrenamiento.java
+++ src/ligaajedrez/vista/ReservarSedeEntrenamiento.java
@@ -5,17 +5,24 @@
  */
 package ligaajedrez.vista;
 
+import javax.swing.JFrame;
+import ligaajedrez.modelo.Administrador;
+
 /**
  *
  * @author Olaf
  */
 public class ReservarSedeEntrenamiento extends javax.swing.JFrame {
-
+    private Administrador administrador;
+    private JFrame previousView;
+    
     /**
      * Creates new form ReservarSedeEntrenamiento
      */
-    public ReservarSedeEntrenamiento() {
+    public ReservarSedeEntrenamiento(Administrador administrador, JFrame previousView) {
         initComponents();
+        this.administrador = administrador;
+        this.previousView = previousView;
     }
 
     /**
@@ -116,47 +123,9 @@ public class ReservarSedeEntrenamiento extends javax.swing.JFrame {
     }//GEN-LAST:event_ReservarActionPerformed
 
     private void AtrasResPartidaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AtrasResPartidaActionPerformed
-        // TODO add your handling code here:
-        MenuJugador menu;
-        menu = new MenuJugador();
-        menu.setVisible(true);
+        previousView.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_AtrasResPartidaActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ReservarSedeEntrenamiento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ReservarSedeEntrenamiento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ReservarSedeEntrenamiento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ReservarSedeEntrenamiento.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ReservarSedeEntrenamiento().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AtrasResPartida;
