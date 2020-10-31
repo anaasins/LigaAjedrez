cc8015fe09f26ad754a3d78a51f69a3c1875d779
Julio Beltran <juliofagotero@gmail.com>
2020-10-31 20:35:05 +0100

    Add administrator and previousView to GestionarEntrenadors

--- src/ligaajedrez/vista/GestionarEntrenadors.java
+++ src/ligaajedrez/vista/GestionarEntrenadors.java
@@ -5,17 +5,24 @@
  */
 package ligaajedrez.vista;
 
+import javax.swing.JFrame;
+import ligaajedrez.modelo.Administrador;
+
 /**
  *
  * @author jbeltran
  */
 public class GestionarEntrenadors extends javax.swing.JFrame {
-
+    private Administrador administrador;
+    private JFrame previousView;
+    
     /**
      * Creates new form GestionarEntrenadors
      */
-    public GestionarEntrenadors() {
+    public GestionarEntrenadors(Administrador administrador, JFrame previousView) {
         initComponents();
+        this.administrador = administrador;
+        this.previousView = previousView;
     }
 
     /**
@@ -109,44 +116,9 @@ public class GestionarEntrenadors extends javax.swing.JFrame {
     }//GEN-LAST:event_updateBtnActionPerformed
 
     private void backHistoryBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backHistoryBtnActionPerformed
-        new MenuAdministrador().setVisible(true);
+        previousView.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_backHistoryBtnActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GestionarEntrenadors.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GestionarEntrenadors.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GestionarEntrenadors.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GestionarEntrenadors.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GestionarEntrenadors().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton backHistoryBtn;
