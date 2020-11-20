a3d5be40fe307a043b7723b756f60fe6010703b9
Marta <Marta@DESKTOP-V52GVC8>
2020-11-20 19:07:29 +0100

    ClubDao

--- src/ligaajedrez/dao/ClubDao.java
+++ src/ligaajedrez/dao/ClubDao.java
@@ -1,12 +1,15 @@
 package ligaajedrez.dao;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import ligaajedrez.modelo.Club;
+import ligaajedrez.modelo.Torneo;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -27,7 +30,7 @@ public class ClubDao {
     public static final String USERNAME = "liga";
     public static final String PASSWORD = "ISIILiga2020";
     
-    FederacionDao federaciondao = new FederacionDao;
+    FederacionDao federaciondao = new FederacionDao();
     /*
         Consultes
     */
@@ -65,6 +68,28 @@ public class ClubDao {
    
     public ClubDao() {}
     
+    public void actualizarClub(Club club) throws ClassNotFoundException, 
+           InstantiationException, IllegalAccessException, SQLException {
+        /*
+        * Conexion a la base de datos
+        */
+        Class.forName(DRIVER).newInstance();
+        Connection oracleConn = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+           
+        oracleConn.setAutoCommit(false);
+        // Sentencia de insert
+        PreparedStatement update = oracleConn.prepareStatement(this.update);
+        
+        update.setInt(1, club.getId());
+        update.setString(2, club.getName());
+        update.setInt(3, club.getFederation().getId());
+        update.setInt(4, club.getSede().getId());
+        update.executeUpdate();
+        
+        oracleConn.commit();
+        oracleConn.setAutoCommit(true);
+        oracleConn.close();
+    }
     public void insertarClub(Club club) throws 
         ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
        
@@ -120,11 +145,87 @@ public class ClubDao {
         ResultSet rs = read.executeQuery();
         
         if (rs.next()) {
+            // Crear llista de tornejos
+            ArrayList<Torneo> torneos = new ArrayList<Torneo>();
+            int [] idTorneos={};            
+            
+            //Buscar els id dels tornejos en els que participa el club
+            idTorneos = TorneoClubDao.leerIntermedios("idClub");
+            
+            //Guardar els tornejos en la llista
+            for(int i=0; i<=idTorneos.length;i++)
+            {
+                torneos.add(torneoDao.leerTorneo(idTorneos[i]));
+            }            
             club.setId(rs.getInt("idClub"));
             club.setName(rs.getString("name"));
             club.setFederation(federacionDao.leerFederacion(rs.getInt("idFederacion")));
-            club.setSede(federacionDao.leerFederacion(rs.getInt("idFederacion")));
+            club.setSede(sedeDao.leerSede(rs.getInt("idSede")));
+            club.setTorneos(torneos);       
         }
         return club;
+    }
+    
+       public ArrayList<Club> leerClubs(int idClub) throws 
+        ClassNotFoundException, InstantiationException, IllegalAccessException, 
+        SQLException, ParseException {
+        
+        ArrayList<Club> clubs = new ArrayList<Club>();
+      
+        Class.forName(DRIVER).newInstance();
+        Connection oracleConn = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+           
+        // Sentencia de insert
+        PreparedStatement read = oracleConn.prepareStatement(select);
+        read.setInt(1, idClub);
+        ResultSet rs = read.executeQuery();
+        
+        while (rs.next()) {
+            
+            // Crear llista de tornejos
+            ArrayList<Torneo> torneos = new ArrayList<Torneo>();
+            int [] idTorneos={};            
+            
+            //Buscar els id dels tornejos en els que participa el club
+            idTorneos = TorneoClubDao.leerIntermedios("idClub");
+            
+            //Guardar els tornejos en la llista
+            for(int i=0; i<=idTorneos.length;i++)
+            {
+                torneos.add(torneoDao.leerTorneo(idTorneos[i]));
+            }
+                     
+            Club  club = new Club(
+            rs.getInt("idClub"),
+            rs.getString("name"),
+            rs.setFederation(federacionDao.leerFederacion(rs.getInt("idFederacion"))),
+            rs.setSede(sedeDao.leerSede(rs.getInt("idFederacion")))
+            );
+            club.setTorneos(torneos);
+            clubs.add(club);
+        }
+        
+        return clubs;
+    }
+       
+     public void borrarClub(int idClub) throws 
+        ClassNotFoundException, 
+        InstantiationException, IllegalAccessException, SQLException {
+        /*
+        * Conexion a la base de datos
+        */
+        Class.forName(DRIVER).newInstance();
+        Connection oracleConn = DriverManager.getConnection(DBURL,USERNAME,PASSWORD);
+            
+        oracleConn.setAutoCommit(false);
+        
+        // Sentencia de borrado
+        PreparedStatement delete = oracleConn.prepareStatement(this.delete);
+        delete.setInt(1, idClub);
+        delete.executeUpdate();
+        
+        oracleConn.commit();
+        oracleConn.setAutoCommit(true);
+        oracleConn.close();
     }
 }
