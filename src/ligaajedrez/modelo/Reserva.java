cb21dd4a9d018bcf247f4984246e3808e980daa5
Julio Beltran <juliofagotero@gmail.com>
2020-11-20 23:33:16 +0100

    Added reservaDao

--- src/ligaajedrez/modelo/Reserva.java
+++ src/ligaajedrez/modelo/Reserva.java
@@ -6,32 +6,17 @@
 package ligaajedrez.modelo;
 
 import java.util.*;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 
 /**
  *
  * @author asins
  */
-@Entity
 public class Reserva {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reservaSeq")
-    @SequenceGenerator(name="reservaSeq",sequenceName="reservaSeq", allocationSize=1, initialValue = 1)
     private int id;
-    @ManyToOne
-    @JoinColumn(name = "usuarioId", referencedColumnName = "id")
     Usuario user;
     Date inicio;
     int hora;
     int contador;
-    @ManyToOne
-    @JoinColumn(name = "sedeId", referencedColumnName = "id")
     private Sede sede;
 
     public Reserva() {}
